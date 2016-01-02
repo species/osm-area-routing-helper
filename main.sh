fc4bca0958acdc36471837208a56bf0065b33cab
Michael Maier <Michael.Maier@student.tugraz.at>
2016-01-03 00:10:09 +0100

    added main script to split up input files and call ksh scripts

---
+++ main.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# written by Michael Maier (s.8472@aon.at)
+# 
+# 02.01.2015   - intial release
+#
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+
+###
+### Standard help text
+###
+
+if [ ! "$1" ] || [ "$1" = "-h" ] || [ "$1" = " -help" ] || [ "$1" = "--help" ]
+then 
+cat <<EOH
+Usage: $0 [OPTIONS] 
+
+$0 is a program to 
+
+OPTIONS:
+   -h -help --help     this help text
+
+EOH
+fi
+
+###
+### variables
+###
+
+
+
+###
+### working part
+###
+
+

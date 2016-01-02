5ff37592d60b842ec8f59e13319027c861ba24fe
Michael Maier <Michael.Maier@student.tugraz.at>
2016-01-03 00:10:18 +0100

    main++

--- main.sh
+++ main.sh
@@ -13,15 +13,17 @@
 ### Standard help text
 ###
 
-if [ ! "$1" ] || [ "$1" = "-h" ] || [ "$1" = " -help" ] || [ "$1" = "--help" ]
+if [ ! "$1" ] || [ ! "$2" ] || [ "$1" = "-h" ] || [ "$1" = " -help" ] || [ "$1" = "--help" ]
 then 
 cat <<EOH
 Usage: $0 [OPTIONS] 
 
-$0 is a program to 
+$0 is a program to add ways connecting all nodes for polygons for a given osm file for better routing
 
 OPTIONS:
    -h -help --help     this help text
+   \$1 the input file: has to be an osm file
+   \$2 output file name
 
 EOH
 fi
@@ -30,10 +32,57 @@ fi
 ### variables
 ###
 
+infile="$1"
+outfile="$2"
 
+tmpdirname="poly-tmp"
 
 ###
 ### working part
 ###
 
+osmconvert "$infile" -o="$infile.o5m"
+osmfilter "$infile.o5m" --keep=" ( highway= and area=yes ) or ( highway= and type=multipolygon ) " --keep-node-tags="all blubb=" -o="$infile.filtered.o5m"
+
+head -n -1 "$infile.areas.osm" > "$outfile"
+
+#1st part: only areas
+osmfilter "$infile.filtered.o5m"  --drop-relations -o="$infile.a1.o5m"
+osmfilter "$infile.a1.o5m" --keep-ways="area=yes"  -o="$infile.areas.osm"
+rm "$infile.a1.o5m"
+
+while read -r line
+do
+  wayid=$(echo "$line" | grep -o '<way id="[0-9-]*"'| cut '-d"' -f 2)
+  if [ ! "$wayid" ]; then
+    continue
+  fi
+
+  osmfilter "$infile.areas.osm" --keep-ways="@id=$wayid"  -o=$tmpdirname/w$wayid.osm
+  ./create_ways.ksh "$tmpdirname/w$wayid.osm" -a "$outfile"
+
+done < "$infile.areas.osm"
+#FIXME not very efficient, calls grep for every line
+# better would be something like this  grep ... | cut | while read  - read somewhere this starts a subshell?
+
+rm "$infile.areas.osm"
+
+#2nd part: MPs: todo
+osmfilter "$infile.filtered.o5m" --keep-way-tags="all blubb=" -o="$infile.mps.osm"
+
+while read -r line
+do
+  relid=$(echo "$line" | grep -o '<relation id="[0-9-]*"'| cut '-d"' -f 2)
+  if [ ! "$relid" ]; then
+    continue
+  fi
+  osmfilter "$infile.mps.osm" --keep="type=multipolygon" --keep-relations="all @id=$relid"  -o="$tmpdirname/r$relid.osm"
+
+
+done < "$infile.mps.osm"
+
+# end
+echo "</osm>" >> "$outfile"
+#rm "$infile.o5m"
+#rm "$infile.filtered.o5m"
 
